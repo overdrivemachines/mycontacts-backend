a5268f55a731df39675eb2653b132c7e4bd36f13
Dipen Chauhan <get.dipen@gmail.com>
2023-06-26 20:30:12 -0700

    responding to requests

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const dotenv = require("dotenv").config();
+const app = express();
+const port = process.env.PORT || 5000;
+
+app.get("/api/contacts", (req, res) => {
+  res.status(200).json({ message: "Get all contacts" });
+  // res.json({ message: "Get all contacts" });
+});
+
+app.listen(port, () => {
+  console.log("server running on port " + port);
+});
